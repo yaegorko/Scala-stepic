90fda2dacfcb8c42b2829996e4e0a796f19f8e6f
yaegorko <yaegorko@gmail.com>
2019-03-20 19:02:02 +0400

    refactor step432 to Stream

--- src/main/scala/org/stepic/step4/step432.scala
+++ src/main/scala/org/stepic/step4/step432.scala
@@ -3,13 +3,6 @@ package org.stepic.step4
 object step432 extends App {
 
   def middle[T](data: Iterable[T]) = {
-
-    val half = data.size / 2: Int
-    val split = data.splitAt(half: Int): (Iterable[T], Iterable[T])
-    val split2 = split._2: Iterable[T]
-    split2.head
+    data.toStream.splitAt(data.size / 2)._2.head
   }
-
-
-
 }
