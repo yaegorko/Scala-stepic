9f90ec00d38b21cd3c8903b40727f9c8bae622e1
yaegorko <yaegorko@gmail.com>
2019-03-21 14:39:11 +0400

    fix tests

--- src/test/scala/org/stepic/step3/step321Test.scala
+++ src/test/scala/org/stepic/step3/step321Test.scala
@@ -3,5 +3,7 @@ package org.stepic.step3
 import org.scalatest.FunSuite
 
 class step321Test extends FunSuite {
-  assert(step321.crispsWeight(BigDecimal("90"), 0.9, 0.1 ) === 10)
+  test("test321") {
+    assert(step321.crispsWeight(BigDecimal("90"), 0.9, 0.1) === 10)
+  }
 }
--- src/test/scala/org/stepic/step5/step511Test.scala
+++ src/test/scala/org/stepic/step5/step511Test.scala
@@ -5,11 +5,16 @@ import java.io.ByteArrayInputStream
 import org.scalatest.FunSuite
 
 class step511Test extends FunSuite {
-  var in = new ByteArrayInputStream("4".getBytes)
-  val result = new java.io.ByteArrayOutputStream()
-  Console.withIn(in) {
-    assert(step511.mutuallySimple() === Console.withOut(result) {
-      print("1 1\n1 2\n1 3\n2 1\n2 3\n3 1\n3 2")
-    })
+  test("test511") {
+    val in = new ByteArrayInputStream("4".getBytes)
+    val result = new java.io.ByteArrayOutputStream()
+    Console.withIn(in) {
+      Console.withOut(result) {
+        step511.mutuallySimple()
+        assert(result.toString == "1 1\n1 2\n1 3\n2 1\n2 3\n3 1\n3 2\n")
+        in.close()
+        result.close()
+      }
+    }
   }
 }
